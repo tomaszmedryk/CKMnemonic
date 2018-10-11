32779e8d33c19e1014d55566bf874d88498bb0aa
Tomasz Mędryk <tomasz.medryk@10clouds.com>
2018-10-12 00:44:44 +0200

    Add Carthage support

---
+++ CKMnemonic/CKMnemonic_iOS/CKMnemonic_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CKMnemonic_iOS.h
+//  CKMnemonic_iOS
+//
+//  Created by Tomasz Mędryk on 11/10/2018.
+//  Copyright © 2018 askcoin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CKMnemonic_iOS.
+FOUNDATION_EXPORT double CKMnemonic_iOSVersionNumber;
+
+//! Project version string for CKMnemonic_iOS.
+FOUNDATION_EXPORT const unsigned char CKMnemonic_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CKMnemonic_iOS/PublicHeader.h>
+
+
